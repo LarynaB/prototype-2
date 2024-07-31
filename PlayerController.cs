1c41efa80117bfce8d292adcf780078d5b980824
LarynaB <lbillingh@gmail.com>
2024-07-31 11:57:21 -0700

    cleaned up code and added additional boundary

--- PlayerController.cs
+++ PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 10.0f;
+    public float xRange = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +15,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {//create vector boundary using if statement for position y and x
-        if (transform.position.x < -10)
+        if (transform.position.x < xRange)
             {
-                transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+                transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
             }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * xRange);
     }
 }
